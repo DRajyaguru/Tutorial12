710c4b404cfa079163b4d999c1303b3a1d4c704d
darshit <drajyaguru698@rku.ac.in>
2020-11-23 09:25:11 +0530

    Initial commit

---
+++ app/src/main/java/com/rku/tutorial12/Util.java
@@ -0,0 +1,9 @@
+package com.rku.tutorial12;
+
+import org.json.JSONArray;
+
+public class Util {
+
+    public static final String URL ="https://reqres.in/api/unknown";
+    public static JSONArray jsonArray = null;
+}
